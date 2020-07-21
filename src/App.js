ea925fde5d2fb751c3a30928276135d90d54d85e
alakaktyc <alakaktyc@gmail.com>
2020-07-21 13:57:09 +0300

    feat: lesson 42, add redux

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Route } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import store from "./redux/state";
+import store from "./redux/store";
 
 const App = (props) => {
     return (
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,25 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Alex'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Oleh'},
+        {id: 4, name: 'Sasnka'},
+        {id: 5, name: 'Viktor'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'MotherFucker'},
+        {id: 3, message: 'Hello'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Sho'}
+    ],
+    newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
